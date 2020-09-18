b924d16f94916dca9e23c9f7ea9012a110872866
DanielC <danielcordero1998@gmail.com>
2020-09-18 02:10:59 -0400

    commit con sass funcional en app.vue

--- src/App.vue
+++ src/App.vue
@@ -8,25 +8,27 @@
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
+<style lang="scss" src="./assets/sass/main.scss">
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
     color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
   }
-}
+
+  #nav {
+    padding: 30px;
+
+    a {
+     font-weight: bold;
+     //  color: #2c3e50;
+     color: rgba(180, 53, 53, 0.815);
+
+      &.router-link-exact-active {
+        color: #42b983;
+     }
+   }
+ }
+
 </style>
