2ab1abeac88c6c11036075b86d3eb5fc341376e6
DanielC <danielcordero1998@gmail.com>
2020-10-07 16:08:34 -0400

    creacion y agregacion de sobre mi

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
+      <Navbar/>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
@@ -8,27 +9,13 @@
   </div>
 </template>
 
-<style lang="scss" src="./assets/sass/main.scss">
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
+<script>
+import Navbar from "@/components/Navbar.vue";
+export default {
+  components: {
+    Navbar
   }
-
-  #nav {
-    padding: 30px;
-
-    a {
-     font-weight: bold;
-     //  color: #2c3e50;
-     color: rgba(180, 53, 53, 0.815);
-
-      &.router-link-exact-active {
-        color: #42b983;
-     }
-   }
- }
-
+};
+</script>
+<style lang="scss"  src="./assets/sass/main.scss">
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="Navbar">
+        <h1>Logo</h1>
+        <h3> Inicio</h3>
+        <h3>Sobre mi</h3>
+        <h3>Contacto</h3>
+    </div>
+</template>
+
+<style lang="scss"  src="./assets/sass/Navbar.scss">
+</style>
